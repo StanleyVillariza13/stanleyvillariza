fcb99a0146f24966251705bd33014c019d520f30
StanleyVillariza13 <116673490+StanleyVillariza13@users.noreply.github.com>
2022-10-26 03:16:40 +0800

    Add files via upload

---
+++ src/main/java/com/demo/VillarizaStanley_1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.demo.VillarizaStanley_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
